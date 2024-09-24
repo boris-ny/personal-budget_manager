53411ecf0e145ce77483c6194d8e012d0d50d834
Boris Nyilindekwe <borisnyilindekwe@gmail.com>
2024-09-24 17:35:57 +0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/ui/button';
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      <Button>Test Button</Button>
+      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+    </div>
+  );
+}
